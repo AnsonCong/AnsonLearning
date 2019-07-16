e7f95d04f6e7870db2541c3fdc864ef8d444cbaf
ansoncong <congkaian@163.com>
2019-07-16 14:22:58 +0800

    add the code in service

--- SpringBatchDemo/src/main/java/com/example/demo/service/ClientInfoServiceImpl.java
+++ SpringBatchDemo/src/main/java/com/example/demo/service/ClientInfoServiceImpl.java
@@ -7,7 +7,24 @@ import com.example.demo.domain.Users;
  * @date 2019/6/9
  */
 public class ClientInfoServiceImpl implements ClientInfoService {
+    @Autowired(name = "updateClientInfoJob")
+    private Job updateClientInfoJob;
+
     @Override
     public void updateUser(Users u) {
+    }
+
+    public boolean triggerUploadClientInfoCsvJob(String fileName, String updateBy) {
+        JobExecution jobExecution = null;
+        try {
+            jobExecution = jobExecution.run(updateClientInfoJob,new JobParameters()
+            .addString("fileName",fileName)
+            .addString("updatedBy",updateBy)
+            .addString("executionTime",new Data())
+            .toJobParameters());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ExitStatus.COMPLETED.equals(jobExecution.getExitStatus());
     }
 }
